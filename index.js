7ca408572c98f6be7e06d6fa9403e80b6fe7d711
Rawle Springer <rspringerdeveloper@gmail.com>
2021-01-20 12:37:01 -0500

    added full basic functionality

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 // basic math operator functions
 let display = "";
+let userInput1 = "";
+let userInput2 = "";
+let userOperatorSelection = "";
+let operatorState = false;
+const numberButtons = document.querySelectorAll(".numbers");
+const operatorButtons = document.querySelectorAll(".operators");
+const displayEl = document.querySelector(".display");
+const equalButton = document.querySelector(".equal");
+const clearButton = document.querySelector(".clear");
 
 const add = (a, b) => {
   return a + b;
@@ -14,6 +23,9 @@ const multiply = (a, b) => {
 };
 
 const divide = (a, b) => {
+  if (b == 0) {
+    prompt("You cannot de");
+  }
   return a / b;
 };
 
@@ -21,16 +33,108 @@ const operate = (operator, num1, num2) => {
   return operator(num1, num2);
 };
 
-const buttons = document.querySelectorAll(".numbers");
-const buttonsArray = Array.from(buttons);
-const displayEl = document.querySelector(".display");
-
-buttonsArray.forEach((button) => {
+numberButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
-    displayEl.textContent = e.target.innerText;
+    //initial render for first digit input
+    if (displayEl.innerText === "0") {
+      display = e.target.innerText;
+      displayEl.textContent = display;
+    } else if (operatorState) {
+      //when user clicks on an operator now focused on second input
+      if (userInput2) {
+        display += e.target.innerText;
+        displayEl.textContent = display;
+        userInput2 = parseInt(displayEl.textContent);
+      } else {
+        display = "";
+        display = e.target.innerText;
+        if (userOperatorSelection == "÷" && display == "0") {
+          alert("You cannot divide by 0");
+          userOperatorSelection = "";
+          return;
+        }
+        displayEl.textContent = display;
+        userInput2 = parseInt(displayEl.textContent);
+      }
+    } else {
+      //continuation of the first user input
+      display += e.target.innerText;
+      displayEl.textContent = display;
+    }
   });
 });
 
-//when a user clicks on a number button
-//attach an event listener on all the buttons
-//put number on the display
+operatorButtons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (!display) {
+      return;
+    }
+
+    if (!userInput1) {
+      userInput1 = parseInt(displayEl.textContent);
+      operatorState = true;
+    }
+
+    userOperatorSelection = e.target.innerText;
+  });
+});
+
+equalButton.addEventListener("click", () => {
+  if (!userInput1 || !userInput2) {
+    return;
+  }
+
+  if (userOperatorSelection === "x") {
+    const result = operate(multiply, userInput1, userInput2);
+    display = result.toString();
+    displayEl.textContent = display;
+    userInput1 = "";
+    userInput2 = "";
+  } else if (userOperatorSelection === "+") {
+    const result = operate(add, userInput1, userInput2);
+    display = result.toString();
+    displayEl.textContent = display;
+    userInput1 = "";
+    userInput2 = "";
+  } else if (userOperatorSelection === "-") {
+    const result = operate(subtract, userInput1, userInput2);
+    display = result.toString();
+    displayEl.textContent = display;
+    userInput1 = "";
+    userInput2 = "";
+  } else {
+    const result = operate(divide, userInput1, userInput2);
+
+    const stringifyResult = result.toString();
+
+    if (stringifyResult.includes(".")) {
+      const decimalNumbers = stringifyResult.split(".");
+
+      if (decimalNumbers[1].length > 13) {
+        display = result.toFixed(13);
+        displayEl.textContent = display;
+        userInput1 = "";
+        userInput2 = "";
+      } else {
+        display = stringifyResult;
+        displayEl.textContent = display;
+        userInput1 = "";
+        userInput2 = "";
+      }
+    } else {
+      display = stringifyResult;
+      displayEl.textContent = display;
+      userInput1 = "";
+      userInput2 = "";
+    }
+  }
+});
+
+clearButton.addEventListener("click", () => {
+  display = "";
+  displayEl.textContent = "0";
+  userInput1 = "";
+  userInput2 = "";
+  userOperatorSelection = "";
+  operatorState = false;
+});
